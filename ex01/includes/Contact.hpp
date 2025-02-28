16649de658d5277d73f6e9276c7fa579bd9f9bdf
Balint Szikora <chamaselion@gmail.com>
2025-02-28 13:41:44 +0100

    Created Contact class and basic setter and getter functions for the variables

---
+++ ex01/includes/Contact.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bszikora <bszikora@student.42helbronn.d    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/28 12:55:55 by bszikora          #+#    #+#             */
+/*   Updated: 2025/02/28 13:39:36 by bszikora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+#include <iostream>
+
+class Contact
+{
+private:
+	std::string first_name;
+	std::string last_name;
+	std::string nickname;
+	std::string phone_number;
+	std::string darkest_secret;
+	
+public:
+	Contact();
+	~Contact();
+
+	// Getter
+	std::string get_first_name() const;
+	std::string get_last_name() const;
+	std::string get_nickname() const;
+	std::string get_phone_number() const;
+	std::string get_darkest_secret() const;
+	// Setter
+	void set_first_name(const std::string &first_name);
+	void set_last_name(const std::string &last_name);
+	void set_nickname(const std::string &nickname);
+	void set_phone_number(const std::string &phone_number);
+	void set_darkest_secret(const std::string &darkest_secret);
+};
+
+
+# endif
