483f1e20d757cc301fffcbe40c0c2da6097bc8b4
Balint Szikora <chamaselion@gmail.com>
2025-03-10 13:51:34 +0100

    Created default constructor and deconstructor as well as added a missing getter function

--- ex02/sources/Account.cpp
+++ ex02/sources/Account.cpp
@@ -6,18 +6,29 @@
 /*   By: bszikora <bszikora@student.42helbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 13:27:25 by bszikora          #+#    #+#             */
-/*   Updated: 2025/03/10 13:47:48 by bszikora         ###   ########.fr       */
+/*   Updated: 2025/03/10 13:50:57 by bszikora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <iostream>
 
+Account::Account()
+{
+	
+}
+Account::~Account()
+{
+	
+}
+
 int Account::getNbAccounts( void ) {return _nbAccounts;}
 int Account::getTotalAmount( void )	{return _totalAmount;}
 int Account::getNbDeposits( void ) {return _totalNbDeposits;}
+int Account::getNbWithdrawals( void ) {return _totalNbWithdrawals;}
 void Account::displayAccountsInfos( void )
 {
 	std::cout << "accounts:" << _nbAccounts << ";total:" << _totalAmount;
 	std::cout << ";deposits" << _totalNbDeposits << ";withdrawals" << _totalNbWithdrawals << "\n";
 }
+
